379effb73953f0a874918298b797ab8c1bc75b68
Manish Meena <105962369+manishm6376@users.noreply.github.com>
2024-08-22 15:29:22 +0000

    dataTypes

---
+++ 01-basic/dataType.js
@@ -0,0 +1,8 @@
+"use strict";//new version of java code 
+// alert(3+3) // we are using nodejs not browsing 
+let name="manish"
+let age = 12
+let accountloggin = true
+console.table([name,age,accountloggin])
+console.log(typeof undefined)
+console.log(typeof null) // null is object type output in js
