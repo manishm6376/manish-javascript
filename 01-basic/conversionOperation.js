34b2735b38f9d6ad3b39677079ecf75836719a8f
Manish Meena <105962369+manishm6376@users.noreply.github.com>
2024-08-22 15:52:51 +0000

    conversionOperation

---
+++ 01-basic/conversionOperation.js
@@ -0,0 +1,9 @@
+let score = null
+console.log(typeof score)
+let numberIs= Number(score)
+console.log(typeof numberIs)
+console.log(numberIs)
+let booleanN= 9
+let booleanNum=Boolean(booleanN)
+console.log(typeof booleanNum)
+console.log(booleanNum)
