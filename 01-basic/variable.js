a635226a191b974a9054f781ca573c25eed08fda
Manish Meena <105962369+manishm6376@users.noreply.github.com>
2024-08-22 15:12:51 +0000

    container config done

---
+++ 01-basic/variable.js
@@ -0,0 +1,13 @@
+const accountId=12345
+let accountEmail="manishm20@iitk.ac.in"
+var accountPassword="2389"
+accountCity="jaipur"
+let accountSate;
+
+accountEmail="manish2002@gmail.com"
+accountPassword="2026"
+accountCity="dehlala"
+
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountSate])
