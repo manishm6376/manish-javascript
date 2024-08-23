5a5a221256e14bab09744b266b83638e5a0c139b
Manish Meena <105962369+manishm6376@users.noreply.github.com>
2024-08-23 13:18:56 +0000

    datatype change

--- 01-basic/datatype-summary.js
+++ 01-basic/datatype-summary.js
@@ -9,3 +9,19 @@ let myObject={
 console.log(typeof array)
 console.log(typeof myFunction)
 console.log(typeof myObject)
+
+// primitive datatype store in stack and non primitive datatype store in heap 
+ let myname="manish"
+ let myanothername=myname
+myanothername="banty"
+ console.log(myname)
+ console.log(myanothername)
+// see the above code , but in non primitive type when we change the other one properties which is holding same refrence memory then in original data  type the value also chnage  
+let myObject1={
+    email: "manish@gmail.com",
+    age: 12,
+}
+let myObject2=myObject1
+myObject2.email="manish@iitk.ac.in"
+console.log(myObject1)
+console.log(myObject2)
