9409448664dd1d2a466ca48587cd53e045375788
Manish Meena <105962369+manishm6376@users.noreply.github.com>
2024-08-23 15:31:07 +0000

    string

---
+++ 01-basic/05-string.js
@@ -0,0 +1,6 @@
+const name=new String('manish')
+const age=23
+console.log(`my name is ${name} and my age is ${age}`)
+const nameT=new String('manish meena is good boy.')
+console.log(nameT.trim())
+console.log(nameT.split(' '))
